65090b0191315396c9cb0c248f4efa2a29d24bbc
PamPaula <pamela.dpaulas@gmail.com>
2021-10-18 15:33:10 -0300

    iniciando testes com jasmine e karma

---
+++ teste_jasmine_karma/src/app/shared/services/unique-id/components/like-widget/like-widget.module.ts
@@ -0,0 +1,16 @@
+import { UniqueIdService } from './../../unique-id.service';
+import { LikeWidgetComponent } from './like-widget.component';
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+
+@NgModule({
+  declarations: [LikeWidgetComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [LikeWidgetComponent],
+  providers: [UniqueIdService]
+})
+export class LikeWidgetModule {}
